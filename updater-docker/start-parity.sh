caf9fb3fe4a595b03bde0bfc2f559cae778fe9f7
Luke Youngblood <luke.youngblood@gmail.com>
2019-01-12 17:00:32 -0800

    Updated to Constantinople.

--- updater-docker/start-parity.sh
+++ updater-docker/start-parity.sh
@@ -7,9 +7,9 @@ then
 	echo "aws s3 sync command failed; exiting."
 	exit 1
 fi
-/parity/parity $* &
+/home/parity/bin/parity $* &
 sleep 10800
-pid=`ps -ef |grep parity/parity|grep -v grep|awk '{print $2}'`
+pid=`ps -ef |grep bin/parity|grep -v grep|awk '{print $2}'`
 kill $pid
 sleep 30
 aws s3 sync --delete --region $region /root/.local/ s3://$bucket/$s3key/.local/
