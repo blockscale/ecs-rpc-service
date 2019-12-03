758cdb63996056d5f5c954596212a0c19b3e461f
Carl Youngblood <carl@youngbloods.org>
2019-12-03 02:05:49 -0800

    Update parity to 2.5.10-stable

--- docker/parity/rpc/start-parity.sh
+++ docker/parity/rpc/start-parity.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 
 mkdir -p /root/.local
-aws s3 sync --delete --region $region s3://$bucket/$s3key/.local/ /root/.local/
+aws s3 sync --delete --region $region s3://$bucket/$s3key/v2.5.10/ /root/.local/
 /bin/parity $*
--- docker/parity/updater/start-parity.sh
+++ docker/parity/updater/start-parity.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 mkdir -p /root/.local
-aws s3 sync --delete --region $region s3://$bucket/$s3key/.local/ /root/.local/
+aws s3 sync --delete --region $region s3://$bucket/$s3key/v2.5.10/ /root/.local/
 if [ $? -ne 0 ]
 then
 	echo "aws s3 sync command failed; exiting."
@@ -12,4 +12,4 @@ sleep 10800
 pid=`ps -ef |grep bin/parity|grep -v grep|awk '{print $2}'`
 kill $pid
 sleep 30
-aws s3 sync --delete --region $region /root/.local/ s3://$bucket/$s3key/.local/
+aws s3 sync --delete --region $region /root/.local/ s3://$bucket/$s3key/v2.5.10/
